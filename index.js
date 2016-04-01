3b203dbb21425c588f5299ed9a68cf2f698d5e96
Jonas Weidler <mail@jonas-weidler.de>
2016-04-01 04:27:14 +0200

    Add tail call optimization, add trampoline pattern

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 'use strict';
 
-module.exports = function (tree, flattenPropertyKey, options) {
+function flattenNodeGenerator(node, parent, index, flattenPropertyKey, settings, stack) {
+    return function flattenNode(list) {
+        node = settings.initNode(node);
+        node.id = settings.generateUniqueId();
+        list.push(node);
+        if (node[flattenPropertyKey]) {
+            for (var i = 0, len = node[flattenPropertyKey].length; i < len; i++) {
+                stack.push(flattenNodeGenerator(node[flattenPropertyKey][i], node, i, flattenPropertyKey, settings, stack));
+            }
+        }
+        if (parent && parent[flattenPropertyKey]) {
+            parent[flattenPropertyKey][index] = node.id;
+        }
+        return list;
+    }
+}
 
+module.exports = function (tree, flattenPropertyKey, options) {
+    var list = [];
+    var stack = [];
     var settings = {
-        initNode: options.initNode || function (node) { return node; },
+        initNode: options.initNode || function (node) {
+            return node;
+        },
         uniqueIdStart: options.uniqueIdStart || 1,
         generateUniqueId: options.generateUniqueId || function () {
             return settings.uniqueIdStart++;
         }
     };
-
-    var list = [];
-
     for (var i = 0, len = tree.length; i < len; i++) {
-        flattenNode(tree[i]);
+        stack.push(flattenNodeGenerator(
+            tree[i], 'root', i, flattenPropertyKey, settings, stack
+        ));
     }
-
-    function flattenNode(node) {
-        node = settings.initNode(node);
-        node.id = settings.generateUniqueId();
-        list.push(node);
-        if (node[flattenPropertyKey]) {
-            var refs = [];
-            for (var i = 0, len = node[flattenPropertyKey].length; i < len; i++) {
-                refs[i] = flattenNode(node[flattenPropertyKey][i]);
-            }
-            node[flattenPropertyKey] = refs;
-        }
-        return node.id;
-    }
-
+    while (stack.length) list = stack.shift()(list);
     return list;
-
 };
