e2fa2f14becd6e8ced0eedefff3c33036086bca4
Jonas Weidler <mail@jonas-weidler.de>
2016-04-03 22:50:28 +0200

    Improve test

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,11 +5,9 @@ var flattenTree = require('./../index');
 
 describe("Flatten tree", function () {
 
-    var tree, result;
-
-    beforeEach(function () {
-
-        tree = [
+    var result;
+    var treeGen = function () {
+        return [
             {
                 name: 'item1',
                 children: [
@@ -22,20 +20,23 @@ describe("Flatten tree", function () {
                     {name: 'item4'}
                 ]
             }
-        ];
+        ]
+    };
+
+    beforeEach(function () {
 
         result = [
-            {id: 1, name: 'item1', children: [2, 4]},
-            {id: 2, name: 'item2', children: [3]},
-            {id: 3, name: 'item3'},
-            {id: 4, name: 'item4'}
+            {id: 1, name: 'item1', children: [2, 3]},
+            {id: 2, name: 'item2', children: [4]},
+            {id: 3, name: 'item4'},
+            {id: 4, name: 'item3'}
         ];
 
     });
 
     it("should match list", function () {
 
-        var list = flattenTree(tree, 'children', {});
+        var list = flattenTree(treeGen(), 'children', {});
 
         expect(list).to.deep.equal(result);
 
@@ -43,6 +44,7 @@ describe("Flatten tree", function () {
 
     it("should not mutate input tree when passed optional initNode() cloning nodes", function () {
 
+        var tree = treeGen();
         var treeClone = JSON.parse(JSON.stringify(tree));
 
         var list = flattenTree(tree, 'children', {
@@ -52,6 +54,37 @@ describe("Flatten tree", function () {
         });
 
         expect(treeClone).to.deep.equal(tree);
+
+    });
+
+    it("should handle deeply nested tree properly", function () {
+
+        this.timeout(100000);
+
+        var treeNode = {
+            name: 'item',
+            children: []
+        };
+
+        result = [];
+
+        function generateChildren(node) {
+            node.children.push(JSON.parse(JSON.stringify(node)));
+            return node.children[0];
+        }
+
+        var node = treeNode;
+
+        for (var i = 0; i < 10000; i++) {
+            node = generateChildren(node);
+            result.push({id: i + 1, name: 'item', children: [i + 2]});
+        }
+
+        result.push({id: i + 1, name: 'item', children: []});
+
+        var list = flattenTree([treeNode], 'children', {});
+
+        expect(result).to.deep.equal(list);
 
     });
 
